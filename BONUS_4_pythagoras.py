f0aad490553821cb3bc896dac2d592fbfff99939
Vojtech Ruzicka <vojtech.ruzicka@merck.com>
2017-04-18 16:11:25 +0200

    Added bonus tasks

---
+++ BONUS_4_pythagoras.py
@@ -0,0 +1,2 @@
+# -*- coding: UTF-8 -*-
+# Napis funkci, ktera ti pro dve zadane delky odvesen pravouhleho trojuhelnika vypocita delku odvesny. (TIP budes potrebovat Pythagorovu vetu)
---
+++ BONUS_5_sum.py
@@ -0,0 +1,2 @@
+# -*- coding: UTF-8 -*-
+# Ptej se uzivatele na cisla, dokud nenapise 'konec'. Potom spocitej sumu (soucet) vsech zadanych cisel.
