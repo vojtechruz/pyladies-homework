d2605a2f5c03478b78b6cf8d66a64628d7ae1542
Vojtech Ruzicka <vojtech.ruzicka@merck.com>
2017-04-18 12:05:38 +0200

    dded first three assignments

---
+++ 02_draw_house.py
@@ -0,0 +1,24 @@
+from turtle import forward, left, right, exitonclick
+from math import sqrt
+
+def draw_house(size):
+    left(45)
+    forward(sqrt(2)*size)
+    left(90)
+    forward(sqrt(2)*size/2)
+    left(90)
+    forward(sqrt(2)*size/2)
+    left(90)
+    forward(sqrt(2)*size)
+    right(135)
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+    forward(size)
+    right(90)
+    forward(size)
+
+house_size = input("Zadej velikost domecku:")
+draw_house(house_size)
+exitonclick()
