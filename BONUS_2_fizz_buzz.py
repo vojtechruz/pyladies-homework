72cdf788f47d7b164ce9bc27fef84a413730682b
Tereza Mikulášová <teagrll@gmail.com>
2017-04-24 22:32:00 +0200

    Splněné bonusové úkoly

--- BONUS_2_fizz_buzz.py
+++ BONUS_2_fizz_buzz.py
@@ -4,3 +4,13 @@
 # Pokud je cislo delitelne soucasne 3 a 5 vypis misto neho retezec "FizzBuzz"
 
 # Bonus: zeptej se uzivatele horni hranici - cislo do ktereho se ma vypisovat (misto 100 napevno)
+
+for cislo in range (int(input("Zadej cislo: "))):
+	if cislo % 15 == 0:
+		print("FizzBuzz")
+	elif cislo % 3 == 0:
+		print("Fizz")
+	elif cislo % 5 == 0:
+		print("Buzz")
+	else:
+		print(cislo)
--- BONUS_5_contains_7.py
+++ BONUS_5_contains_7.py
@@ -1,2 +1,7 @@
 # -*- coding: UTF-8 -*-
 # Vypis vsechna cela cisla od 1 do 1000, ktera obsahuji cislici 7.
+
+for cislo in range(1001):
+	cislo = str(cislo)
+	if "7" in cislo:
+		print cislo
